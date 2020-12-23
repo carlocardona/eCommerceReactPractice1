1864a25cce290c6e5347f090be0e351dcb9afd4d
Student <student@students-mbp.lan>
2020-12-23 19:29:57 +1100

    moved items to react enviro

--- src/App.js
+++ src/App.js
@@ -3,22 +3,73 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="grid-container">
+        <header className="header">
+            <div className="brand">
+                <button onClick="openMenu()">
+                    &#9776;
+                </button>
+                <a href="index.html">SpeakEasyLA</a>
+            </div>
+            <div className="header-links">
+                <a href="cart.html">Cart</a>
+                <a href="sigin.html">Sign In</a>
+            </div>
+        </header>
+
+        <aside className="sidebar">
+            <h3>Categories</h3>
+            <button className="sidebar-close-button" onclick="closeMenu()">x</button>
+            <ul>
+                <li>
+                   <a href="index.html">One Seater</a>
+                </li>
+                <li>
+                    <a href="index.html">Multiple Seater</a>
+                 </li>
+            </ul>
+        </aside>
+        <main className="main">
+            <div className="content">
+                <ul className="products">
+                    <li>
+                        <div className="product">
+                            <img className="product-image" src="/images/Screenshot_2020-12-23 Speakeasy 👄 ( thespeakeasyla) • Instagram photos and videos(1).jpg" alt="couchone"/>
+                            <div className="product-name">
+                               <a href="product.html">Dark Tan Couch</a>
+                            </div>
+                            <div className="product-brand">Mercadillo</div>
+                            <div className="product-price">$2500</div>
+                            <div className="product-rating">4.5 Stars (10 Reviews)</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="product">
+                            <img className="product-image" src="/images/Screenshot_2020-12-23 Speakeasy 👄 ( thespeakeasyla) • Instagram photos and videos(2).jpg" alt="couchone"/>
+                            <div className="product-name">
+                               <a href="product.html">Single Tan</a>
+                            </div>
+                            <div className="product-brand">Mercadillo</div>
+                            <div className="product-price">$999</div>
+                            <div className="product-rating">4.5 Stars (10 Reviews)</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="product">
+                            <img className="product-image" src="/images/Screenshot_2020-12-23 Speakeasy 👄 ( thespeakeasyla) • Instagram photos and videos.jpg" alt="couchone"/>
+                            <div className="product-name">
+                               <a href="product.html">Maroon Couch</a>
+                            </div>
+                            <div className="product-brand">Mercadillo</div>
+                            <div className="product-price">$1999</div>
+                            <div className="product-rating">4.5 Stars (10 Reviews)</div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </main>
+        <footer className="footer">All rights reserved.</footer>
+        </div>
   );
 }
 
