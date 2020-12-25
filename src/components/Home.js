db35b0271f46e1105563c1cd730a06c2b5aeb943
Student <student@students-mbp.lan>
2020-12-25 15:36:35 +1100

    finished front end

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,7 +16,7 @@ function Home (props) {
                <Link to={'/product/' + product._id}>{product.name}</Link>
             </div>
             <div className="product-brand">{product.brand}</div>
-            <div className="product-price">{product.price}</div>
+            <div className="product-price">${product.price}</div>
             <div className="product-rating">{product.ratings} Stars {product.reviews}</div>
         </div>
       </li>)
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,13 +1,61 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import data from '../data';
+
 
 function Products (props) {
     console.log(props.match.params.id);
     const product = data.products.find(x => x._id === props.match.params.id);
 
-    return <div>
-        <h1>{product.name}</h1>
-    </div>
+    return (
+        <div>
+            <div className="back-to-result">
+                <Link to="/">Back to result</Link>
+            </div>
+            <div className="details">
+                <div className="details-image">
+                    <img src={product.image} alt="product"></img>
+                </div>
+                <div className="details-info">
+                    <ul>
+                        <li>
+                            <h4>{product.name}</h4>
+                        </li>
+                        <li>
+                            {product.rating}Stars ({product.numReviews} Reviews)
+                        </li>
+                        <li>
+                            Price: <b>${product.price}</b>
+                        </li>
+                        <li>
+                            Description:
+                            <div>{product.description}</div>
+                        </li>
+                    </ul>
+                </div>
+                <div className="details-action">
+                    <ul>
+                        <li>
+                            Price: {product.price}
+                        </li>
+                        <li>
+                            Status: {product.status}
+                        </li>
+                        <li>
+                            Qty: <select>
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                </select>
+                        </li>
+                        <li>
+                            <button className="button">Add to Cart</button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Products;
